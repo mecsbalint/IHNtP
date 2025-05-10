ecc36424e9032357ec9dae6ffd3f0ec1ec60e28b
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-05-10 19:19:14 +0200

    Setup Spring Security and implement UserEntity

---
+++ ihntp-backend/src/main/java/com/mecsbalint/backend/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.mecsbalint.backend.security.service;
+
+
+import com.mecsbalint.backend.model.UserEntity;
+import com.mecsbalint.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private  final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email));
+
+        List<SimpleGrantedAuthority> roles = userEntity.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .toList();
+
+        return new User(userEntity.getName(), userEntity.getPassword(), roles);
+    }
+}
