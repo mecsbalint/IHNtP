75551f0c68528aa327bfec4a22e1aa98676a6528
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-06-29 11:18:47 +0200

    File saving directory is set with env variable

--- ihntp-backend/src/main/java/com/mecsbalint/backend/configuration/WebConfig.java
+++ ihntp-backend/src/main/java/com/mecsbalint/backend/configuration/WebConfig.java
@@ -1,17 +1,21 @@
 package com.mecsbalint.backend.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${mecsbalint.app.file-upload-dir}")
+    private String uploadDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/api/images/**")
-                .addResourceLocations("classpath:/static/")
+                .addResourceLocations(String.format("file:/%s", uploadDir))
                 .setCachePeriod(0);
     }
 }
